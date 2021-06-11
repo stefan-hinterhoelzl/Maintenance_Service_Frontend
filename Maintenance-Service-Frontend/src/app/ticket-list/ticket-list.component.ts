74214dfafe23f8f1d77f57a1a084a990e893c0fd
deutho <59829810+deutho@users.noreply.github.com>
2021-06-11 19:15:05 +0200

    quciksave - deleting ticket-list progress... trying new solution

--- Maintenance-Service-Frontend/src/app/ticket-list/ticket-list.component.ts
+++ Maintenance-Service-Frontend/src/app/ticket-list/ticket-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Priority, Ticket } from '../model/model';
+import { AlertService } from '../services/alertService';
+import { ApiService } from '../services/api-service';
 
 @Component({
   selector: 'app-ticket-list',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketListComponent implements OnInit {
 
-  constructor() { }
+  result: any;
+  tickets: Ticket[] = [];
+  constructor(private api: ApiService, private alert: AlertService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(){
+    this.result = await this.api.getAllTickets().catch( () => {
+      this.alert.error("Tickets not accessable")
+    })
+
+    if(this.result._embedded != null){
+      this.tickets = this.result._embedded.ticketList
+    }
+   
+
+  }
+
+  sortList(parameter: string){
+    let copiedList = []
+    this.tickets.forEach(val => copiedList.push(Object.assign({}, val)));
+
+    if(parameter == "id"){
+      this.tickets.sort((a,b) => a.id - b.id)
+    }
+
+    if(parameter == "title"){
+      this.tickets.sort((a,b) => {
+        if(a.title < b.title) { return -1; }
+        if(a.title > b.title) { return 1; }
+        return 0;
+      })
+    }
+
+    if(parameter == "priority"){
+      const priorityOrder = Object.values(Priority);
+      this.tickets.sort((a,b) => {
+       return priorityOrder.indexOf(a.priority) - priorityOrder.indexOf(b.priority)
+      })
+    }
+
+
+
+
+    let arraysAreUnchange: boolean = true
+    let i = 0;
+    this.tickets.forEach(ticket => {
+      if(ticket != copiedList[i]){
+        arraysAreUnchange = false
+      }
+      i++;
+    })
+    console.log(arraysAreUnchange)
   }
 
 }
